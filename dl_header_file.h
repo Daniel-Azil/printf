93492ab24b861062b67ee0c2047b16a7b516aca0
Daniel Azil <danielazil.fse@gmail.com>
2023-06-27 06:36:55 +0000

    Added new prototype functions to the header file

--- dl_header_file.h
+++ dl_header_file.h
@@ -12,7 +12,11 @@
 int _printf(const char *format, ...);
 int handles_buffer(char *_buff_cache, int size);
 char *int_string(int al, int size, int var);
-ar *d_custom_specifier(va_list catalog_ls);
+char *d_custom_specifier(va_list catalog_ls);
 char *c_custom_specifier(va_list catalog_ls);
+
+char *str_reverse_func(va_list catalog_ls);
+char *copy_string(char *dga_var, char *_source);
+char *cipher_rot13_func(va_list catalog_ls);
 
 #endif
