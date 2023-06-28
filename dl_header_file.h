ce6ebc6f4e6f0d56ca39f7dc16dc729133d1242a
Lukma Mputa <aziwemputa@gmail.com>
2023-06-28 00:29:13 +0000

    Added new functions to the header file

--- dl_header_file.h
+++ dl_header_file.h
@@ -18,5 +18,13 @@ char *c_custom_specifier(va_list catalog_ls);
 char *str_reverse_func(va_list catalog_ls);
 char *copy_string(char *dga_var, char *_source);
 char *cipher_rot13_func(va_list catalog_ls);
+nt get_string_length(char *_string);
+char *convert_to_base8(va_list catalog_ls);
+char *s_custom_specifier(va_list catalog_ls);
 
+void process_output_buffer(char *_buff_cache, int size, va_list catalog_ls);
+char* (*find_conversion_function(char idnt))(va_list);
+char *allocate_output_buffer(void);
+
+char *convert_to_base2(va_list catalog_ls);
 #endif
